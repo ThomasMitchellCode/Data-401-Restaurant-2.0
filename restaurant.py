707cb8ada8ba2a2dbd106f14e60a8225f58d82bf
Tommy Mitchell <tmitchell@spartaglobal.com>
2024-03-06 16:23:38 +0000

    First changes made. test_order now passes

--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,17 @@
 class Table:
-    pass
+    bill = []
+
+    def __init__(self, number_of_diners):
+        self.number_of_diners = number_of_diners
+        self.bill = []
+
+# new_table = Table(3)
+# print(type(new_table))
+# print(new_table.number_of_diners)
+
+    def order(self, item, price, quantity=1):
+        order_item = {}
+        order_item["item"] = item
+        order_item["price"] = price
+        order_item["quantity"] = quantity
+        self.bill.append(order_item)
