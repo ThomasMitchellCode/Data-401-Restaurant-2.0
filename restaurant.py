146a074e5cd4c5a25163ad7b62462ec3725f5910
Usama <usama.asghar@hotmail.co.uk>
2024-03-07 14:52:09 +0000

    All Tests Passed

--- restaurant.py
+++ restaurant.py
@@ -2,8 +2,8 @@ class Table:
 
     # bill = []  # instance variable called bill that is a list
 
-    def __init__(self, people):
-        self.people = people,
+    def __init__(self, people: int):
+        self.people = people
         self.bill = []
 
     def order(self, item, price, quantity=1):
@@ -21,3 +21,19 @@ class Table:
             if order["item"] == item and order["price"] == price:
                 order["quantity"] = quantity - 1
 
+    def get_subtotal(self):
+        sub_total = 0
+        for order in self.bill:
+            sub_total += order["price"] * order["quantity"]
+        return sub_total
+
+    def get_total(self, service_percent=0.1):
+        sub_total = self.get_subtotal()
+        service_charge = sub_total * service_percent
+        total = sub_total + service_charge
+        return {"Sub Total": "£" + "%.2f" % sub_total,
+                "Service Charge": "£" + str(service_charge),
+                "Total": "£" + str(total)}
+
+    def split_bill(self):
+        return round(self.get_subtotal() / self.people, 2)
