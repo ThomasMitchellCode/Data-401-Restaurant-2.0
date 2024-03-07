150d18ba99077b476360fccf1ec3da6ecff4292b
JackXLin <jack.llll@yahoo.co.uk>
2024-03-07 13:37:07 +0000

    Improved restaurant.py to pass 4/7 tests

--- restaurant.py
+++ restaurant.py
@@ -3,14 +3,23 @@ class Table:
         self.num_of_diners = num_of_diners
         self.bill = list()
 
-    def order (self, item: str, price: float, quantity=1):
-        for i in self.bill:
-            if item in i["item"] and price in i["price"]:
-                i["quantity"] += 1
-            else:
-                self.bill.append({"item": item, "price": price, "quantity": quantity})
+    # def order(self, item: str, price: float, quantity=1):
+    #     for i in self.bill:
+    #         if item in i["item"] and price in i["price"]:
+    #             i["quantity"] += 1
+    #         else:
+    #             self.bill.append({"item": item, "price": price, "quantity": quantity})
 
-    def remove (self, item: str, price:int):
+    def order(self, item: str, price: float, quantity=1):
+        for i in self.bill:
+            if item == i["item"] and price == i["price"]:
+                i["quantity"] += quantity
+                break
+        else:
+            # If the item doesn't exist, add a new entry
+            self.bill.append({"item": item, "price": price, "quantity": quantity})
+
+    def remove(self, item: str, price: float):
         for i in self.bill:
             if item in i["item"] and price in i["price"]:
                 self.bill.pop(self.bill.index(i))
@@ -18,17 +27,17 @@ class Table:
         else:
             return False
 
-    def get_subtotal (self):
+    def get_subtotal(self):
         subtotal = 0
         for i in self.bill:
             subtotal += i["price"] * i["quantity"]
-        return subtotal
+        return round(subtotal, 2)
 
     def get_total(self, tip_percentage=0.1):
         sub_total = self.get_subtotal()
         service_charge = sub_total*tip_percentage
         total = sub_total + service_charge
-        return {"Sub Total": sub_total, "Service Charge": service_charge, "Total": total}
+        return {"Sub Total": "£"+str(sub_total), "Service Charge": "£"+str(service_charge), "Total": "£"+str(total)}
 
     def split_bill(self):
         return round(self.get_subtotal() / self.num_of_diners, 2)
