eb4533f6ee78dece1f062f20cd714ac647e1863a
Usama <usama.asghar@hotmail.co.uk>
2024-03-07 11:44:22 +0000

    order tests and remove tests passed

--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,23 @@
 class Table:
-    pass
+
+    # bill = []  # instance variable called bill that is a list
+
+    def __init__(self, people):
+        self.people = people,
+        self.bill = []
+
+    def order(self, item, price, quantity=1):
+        order_item = {"item": item, "price": price, "quantity": quantity}
+        self.bill.append(order_item)
+
+        # for order in self.bill:
+        #     if order_item["item"] == item and order_item["price"] == price:
+        #         quantity += 1
+        #     else:
+        #         self.bill.append(order_item)
+
+    def remove(self, item, price, quantity):
+        for order in self.bill:
+            if order["item"] == item and order["price"] == price:
+                order["quantity"] = quantity - 1
+
