d3117326c1b8696f2e263eacffd45da8ae7e4ac8
Muhammad Nawaz <nawazm621@outlook.com>
2024-03-07 14:26:09 +0000

    initial commit, added order method, passed 2 tests

--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,15 @@
 class Table:
-    pass
+    def __init__(self, num_diners):
+        self.num_diners = num_diners
+        self.bill = []
+
+    def order(self, item, price, quantity=1):
+
+        for order in self.bill:
+            if order["item"] == item and order["price"] == price:
+                order["quantity"] += quantity
+                break
+
+        else:
+
+            self.bill.append({"item": item, "price": price, "quantity": quantity})
