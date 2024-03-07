a16e74acf4d8883b43beb1a9b1e756f76fe9b953
JackXLin <jack.llll@yahoo.co.uk>
2024-03-07 14:22:26 +0000

    fixed restaurant.py to pass all tests

--- restaurant.py
+++ restaurant.py
@@ -19,13 +19,15 @@ class Table:
             # If the item doesn't exist, add a new entry
             self.bill.append({"item": item, "price": price, "quantity": quantity})
 
-    def remove(self, item: str, price: float):
+    def remove(self, item: str, price: float, quantity: int):
         for i in self.bill:
-            if item in i["item"] and price in i["price"]:
-                self.bill.pop(self.bill.index(i))
+            if item == i["item"] and price == i["price"] and i["quantity"] > quantity:
+                i["quantity"] -= quantity
                 return True
-        else:
-            return False
+            elif item == i["item"] and price == i["price"] and i["quantity"] == quantity:
+                self.bill.pop(self.bill.index(i))
+            else:
+                return False
 
     def get_subtotal(self):
         subtotal = 0
@@ -35,12 +37,10 @@ class Table:
 
     def get_total(self, tip_percentage=0.1):
         sub_total = self.get_subtotal()
-        service_charge = sub_total*tip_percentage
+        service_charge = sub_total * tip_percentage
         total = sub_total + service_charge
-        return {"Sub Total": "£"+str(sub_total), "Service Charge": "£"+str(service_charge), "Total": "£"+str(total)}
+        return {"Sub Total": "£" + "%.2f" % sub_total, "Service Charge": "£" + str(service_charge),
+                "Total": "£" + str(total)}
 
     def split_bill(self):
         return round(self.get_subtotal() / self.num_of_diners, 2)
-
-
-
